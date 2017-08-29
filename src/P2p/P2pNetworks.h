2a1b6726748a1f4857eed65200944ec99812b3c3
egynw <egynw@hotmail.com>
2017-08-29 21:37:48 +0200

    Update P2pNetworks.h

--- src/P2p/P2pNetworks.h
+++ src/P2p/P2pNetworks.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2011-2016 The Cryptonote developers
-// Copyright (c) 2014-2017 XDN-project developers
+// Copyright (c) 2017 Ultranote developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -7,5 +7,5 @@
 
 namespace CryptoNote
 {
-  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x4E, 0x6F, 0x73, 0x63, 0x65, 0x20, 0x74, 0x65, 0x20, 0x69, 0x70, 0x73, 0x75, 0x6D, 0x20, 0x20 } };
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0xA0, 0xA1, 0x1A, 0xA1, 0xA1, 0x0A, 0xA1, 0xA0, 0xA0, 0x1A, 0x0A, 0x1A, 0xA1, 0x1A, 0x1A, 0xA1 } };
 }
